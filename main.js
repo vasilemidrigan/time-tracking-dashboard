98690067255d70664fed068a27c76f038995e878
Vasile Midrigan <111rigan111@gmail.com>
2022-02-19 06:17:44 +0000

    working on profile section

---
+++ main.js
@@ -0,0 +1,13 @@
+"use strict";
+let jsonData;
+// Getting data from .json file
+fetch("data.json")
+  .then((response) => {
+    return response.json();
+  })
+  .then((jsonResult) => (jsonData = jsonResult))
+  .catch((err) => new Error(console.log(err)));
+
+setTimeout(() => {
+  console.log(jsonData);
+}, 1000);
