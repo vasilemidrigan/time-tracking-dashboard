43d2c805adeea2fae4261272ec759091cf5ce256
Vasile Midrigan <111rigan111@gmail.com>
2022-02-20 17:11:05 +0000

    add _position-absolute.scss, complete switch data implementation

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
 "use strict";
-
 // Variables
-let jsonData;
 const bottomSection = document.querySelector(".bottom-section");
 const bottomSectionChildrens = [...bottomSection.children];
-
+// Activities
 const work = document.querySelector(".work");
 const play = document.querySelector(".play");
 const study = document.querySelector(".study");
 const exercise = document.querySelector(".exercise");
 const social = document.querySelector(".social");
 const selfCare = document.querySelector(".self-care");
-
 const activities = [work, play, study, exercise, social, selfCare];
+// Current and Previous Hours
 let currentHours = document.querySelectorAll(".time-dedication");
 let previousText = document.querySelectorAll(".previous__text");
+// for JSON extracting data
+let jsonData;
+let daily = [];
+let weekly = [];
+let monthly = [];
 
-// Fetching JSON data
-
+// Fetching JSON object
 fetch("data.json")
   .then((response) => {
     return response.json();
@@ -25,49 +27,78 @@ fetch("data.json")
   .then((jsonResult) => (jsonData = jsonResult))
   .catch((err) => new Error(console.log(err)));
 
+// In order to work with .json object, we're running this code below
+//   after 1,5s, at this time it will be already fetched.
 setTimeout(() => {
-  console.log(jsonData);
   // Extracting daily, weekly and monthly hours from .json
-  let daily = [];
-  let weekly = [];
-  let monthly = [];
   jsonData.forEach((el) => {
     daily.push(el.timeframes.daily);
     weekly.push(el.timeframes.weekly);
     monthly.push(el.timeframes.monthly);
   });
 
-  // Getting current hours data
-  let currentHoursContent = Array.from(currentHours).map(
-    (el) => el.textContent
-  );
-
-  //               CONSOLE LOG
-  console.log("currentHoursContent:", currentHoursContent);
-
-  // Update Activity Card Content
+  // Update Activity Cards Content
   function updateActivity(activity) {
-    if (activity === "Weekly") {
-      for (let i = 0; i < currentHoursContent.length; i++) {
-        currentHours[i].textContent = weekly[i].current;
-        previousText[i].textContent = weekly[i].previous;
-      }
-    } else if (activity === "Daily") {
-      for (let i = 0; i < currentHoursContent.length; i++) {
+    // Daily Hours
+    if (activity === "Daily") {
+      for (let i = 0; i < currentHours.length; i++) {
+        // Updating current hours data - daily
         currentHours[i].textContent = daily[i].current;
+        if (currentHours[i].textContent == 1) {
+          currentHours[i].textContent += " hr";
+        } else {
+          currentHours[i].textContent += " hrs";
+        }
+        // Update previous hours data - daily
         previousText[i].textContent = daily[i].previous;
+        if (previousText[i].textContent == 1) {
+          previousText[i].textContent = `Yesterday - ${daily[i].previous}hr`;
+        } else {
+          previousText[i].textContent = `Yesterday - ${daily[i].previous}hrs`;
+        }
       }
+      // Weekly Hours
+    } else if (activity === "Weekly") {
+      for (let i = 0; i < currentHours.length; i++) {
+        // Update current hours data - weekly
+        currentHours[i].textContent = weekly[i].current;
+        if (currentHours[i].textContent == 1) {
+          currentHours[i].textContent += " hr";
+        } else {
+          currentHours[i].textContent += " hrs";
+        }
+        // Update previous hours data - weekly
+        previousText[i].textContent = weekly[i].previous;
+        previousText[i].textContent = daily[i].previous;
+        if (previousText[i].textContent == 1) {
+          previousText[i].textContent = `Last Week - ${weekly[i].previous}hr`;
+        } else {
+          previousText[i].textContent = `Last Week - ${weekly[i].previous}hrs`;
+        }
+      }
+      // Monthly hours
     } else {
-      for (let i = 0; i < currentHoursContent.length; i++) {
+      for (let i = 0; i < currentHours.length; i++) {
+        // Update current hours data - monthly
         currentHours[i].textContent = monthly[i].current;
+        if (currentHours[i].textContent == 1) {
+          currentHours[i].textContent += " hr";
+        } else {
+          currentHours[i].textContent += " hrs";
+        }
+        // Update previous hours data - monthly
         previousText[i].textContent = monthly[i].previous;
+        if (previousText[i].textContent == 1) {
+          previousText[i].textContent = `Last Month - ${monthly[i].previous}hr`;
+        } else {
+          previousText[
+            i
+          ].textContent = `Last Month - ${monthly[i].previous}hrs`;
+        }
       }
     }
   }
-
-  // Events:
-
-  // Switching active states of the bottom-section periods
+  // Switch data event
   bottomSectionChildrens.forEach((el) => {
     el.addEventListener("click", () => {
       // Remove active state from previous element
@@ -80,4 +111,4 @@ setTimeout(() => {
       updateActivity(el.textContent);
     });
   });
-}, 1000);
+}, 1100);
